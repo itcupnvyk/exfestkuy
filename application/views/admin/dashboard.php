3a76c5cbfabbdb2a3ba9ce108ad707e88293fdf4
refandas <refanda10ss@gmail.com>
2019-10-19 16:23:13 +0700

    adding home, stand view and editing dashboard

--- application/views/admin/dashboard.php
+++ application/views/admin/dashboard.php
@@ -21,29 +21,29 @@
         <h2 class="text-center my-4">Daftar Stand</h2>
         <ul class="list-group">
             <li class="list-group-item">
-                <a class="text-decoration-none" href="stand_dashboard.html"><span>Stand Pertama</span></a>
+                <a class="text-decoration-none" href="stand_dashboard.php"><span>Stand Pertama</span></a>
             </li>
             <li class="list-group-item">
-                <a class="text-decoration-none" href="stand_dashboard.html"><span>Stand Kedua</span></a>
+                <a class="text-decoration-none" href="stand_dashboard.php"><span>Stand Kedua</span></a>
             </li>
             <li class="list-group-item">
-                <a class="text-decoration-none" href="stand_dashboard.html"><span>Stand Ketiga</span></a>
+                <a class="text-decoration-none" href="stand_dashboard.php"><span>Stand Ketiga</span></a>
             </li>
         </ul>
     </div>
     <div class="text-right fixed-bottom" id="button-plus">
-        <a class="text-decoration-none" href="stand_dashboard.html"><i class="icon-plus"></i></a>
+        <a class="text-decoration-none" href="stand_dashboard.php"><i class="icon-plus"></i></a>
     </div>
     <nav class="navbar navbar-light navbar-expand-md" id="nav-footer">
         <div class="container-fluid">
-            <a class="nav-link" href="stand.html">Stand</a>
-            <a class="nav-link" href="product.html">Produk</a>
+            <a class="nav-link" href="stand.php">Stand</a>
+            <a class="nav-link" href="product.php">Produk</a>
             <a class="nav-link" href="#">Peta</a>
         </div>
     </nav>
     <nav class="navbar navbar-light navbar-expand-md fixed-top">
         <div class="container-fluid">
-            <a href="index.html"><img class="img-fluid" src="assets/img/Logo%20exfest.png" width="100"></a>
+            <a href="index.php"><img class="img-fluid" src="assets/img/Logo%20exfest.png" width="100"></a>
             <button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1">
                 <span class="sr-only">Toggle navigation</span>
                 <span class="navbar-toggler-icon"></span>
