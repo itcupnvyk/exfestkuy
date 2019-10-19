36a4ff960004d15fa343e6b6af5e26f9aa16a12b
refandas <refanda10ss@gmail.com>
2019-10-19 16:45:27 +0700

    add map, product, and login view

---
+++ application/views/map/map.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Exfeskuy</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i">
+    <link rel="stylesheet" href="assets/fonts/fontawesome-all.min.css">
+    <link rel="stylesheet" href="assets/fonts/ionicons.min.css">
+    <link rel="stylesheet" href="assets/fonts/simple-line-icons.min.css">
+    <link rel="stylesheet" href="assets/css/Footer-Basic.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.8.2/css/lightbox.min.css">
+    <link rel="stylesheet" href="assets/css/Login-Form-Dark.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<body>
+    <nav class="navbar navbar-light navbar-expand-md fixed-top">
+        <div class="container-fluid">
+            <a href="index.html"><img class="img-fluid" src="assets/img/Logo%20exfest.png" width="100"></a>
+            <button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div
+                class="collapse navbar-collapse" id="navcol-1">
+                <ul class="nav navbar-nav ml-auto">
+                    <li class="nav-item" role="presentation">
+                        <a class="nav-link" href="stand.html">Stand</a>
+                    </li>
+                    <li class="nav-item" role="presentation">
+                        <a class="nav-link" href="#">Produk</a>
+                    </li>
+                    <li class="nav-item" role="presentation">
+                        <a class="nav-link active" href="#">Peta</a>
+                    </li>
+                </ul>
+        </div>
+        <div class="text-right" id="button-search"><a class="text-decoration-none" href="#"><i class="fas fa-search" data-toggle="modal" data-target="#modal-search"></i></a></div>
+        </div>
+    </nav>
+
+    <div class="container" id="content">
+        <h2 class="text-center my-4">Peta Stand</h2>
+        <img class="img-fluid" src="assets/img/Exfeskuy_Denah1.jpg">
+    </div>
+
+    <div class="footer-basic pt-1 pb-4">
+        <footer class="mb-5">
+            <p class="copyright">Powered by ANTHE and POSPERO</p>
+        </footer>
+    </div>
+
+    <nav class="navbar navbar-light navbar-expand-md" id="nav-footer">
+        <div class="container-fluid">
+            <a class="nav-link" href="stand.html">Stand</a>
+            <a class="nav-link" href="product.html">Produk</a>
+            <a class="nav-link" href="#">Peta</a>
+        </div>
+    </nav>
+
+
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.8.2/js/lightbox-plus-jquery.min.js"></script>
+    <script src="assets/js/theme.js"></script>
+</body>
+
+</html>
