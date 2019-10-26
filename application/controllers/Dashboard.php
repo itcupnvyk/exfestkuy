9ed7b5e19eb3b6c2860c0e84c692d605413393b1
refandas <refanda10ss@gmail.com>
2019-10-26 09:18:22 +0700

    add dashboard controller

---
+++ application/controllers/Dashboard.php
@@ -0,0 +1,128 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Dashboard extends CI_Controller {
+    
+    public function __construct() {
+        parent::__construct();
+        $this->load->model('UserModel');
+        $this->load->model('ProductModel');
+        $this->load->library('session');
+    }
+
+    public function index() {
+        $data = array(
+            'title'     => 'Dashboard',
+            'users'     => $this->UserModel->findAll()
+        );
+
+        $this->load->view('templates/header', $data);
+        $this->load->view('dashboard/dashboard', $data);
+        $this->load->view('templates/footer_navbar');
+        $this->load->view('templates/footer');
+    }
+
+    public function addStand() {
+        $this->load->view('templates/header');
+        $this->load->view('stand/stand_add');
+        $this->load->view('templates/footer');
+    }
+
+    public function createStand() {
+        $avatar_url = base_url('assets/img/avatar_default.svg');
+        $name       = $this->input->post('name');
+        $email      = $this->input->post('email');
+        $password   = $this->input->post('password');
+        $map_url    = base_url('assets/img/Exfeskuy_Denah.jpg');
+        $role_id    = 2;
+
+        $user = array(
+            'avatar_url'    => $avatar_url,
+            'name'          => $name,
+            'email'         => $email,
+            'password'      => $password,
+            'map_url'       => $map_url,
+            'role_id'       => $role_id
+        );
+
+        $this->UserModel->create($user);
+    }
+
+    public function stand($id_stand) {
+        $data = array(
+            'title'     => 'Update Stand',
+            'user'      => $this->UserModel->findByID($id_stand)
+        );
+
+        $this->load->view('templates/header', $data);
+        $this->load->view('stand/stand_update', $data);
+        $this->load->view('templates/footer');
+    }
+
+    public function updateStand($id_stand) {
+        $name       = $this->input->post('name');
+        $map_url    = $this->input->post('map');
+
+        $user = array(
+            'id'        => $id_stand,
+            'name'      => $name,
+            'map_url'   => base_url('assets/img/') . $map_url . '.jpg'
+        );
+        
+        $this->UserModel->update($user);
+        redirect('dashboard');
+    }
+
+    public function products($user_id) {
+        $data = array(
+            'title'     => 'Update Produk',
+            'user_id'   => $user_id,
+            'products'  => $this->ProductModel->findByUserID($user_id),
+            'user'      => $this->UserModel->findByID($user_id)
+        );
+
+        $this->load->view('templates/header', $data);
+        $this->load->view('products/product_add', $data);
+        $this->load->view('templates/footer');
+    }
+
+    public function updateProduct($product_id) {
+        $name   = $this->input->post('name');
+        $price  = $this->input->post('price');
+
+        $product = array(
+            'id'    => $product_id,
+            'name'  => $name,
+            'price' => $price
+        );
+
+        $this->ProductModel->update($product);
+        redirect('dashboard/products/' . $product_id);
+    }
+
+    public function addProduct($user_id) {
+        $name           = $this->input->post('name');
+        $description    = " ";
+        $quantity       = 1;
+        $price          = $this->input->post('price');
+        $image_url      = " ";
+        $user_id        = $user_id;
+        
+        $products = array(
+            'name'          => $name,
+            'description'   => $description,
+            'quantity'      => $quantity,
+            'price'         => $price,
+            'image_url'     => $image_url,
+            'user_id'       => $user_id
+        );
+
+        $this->ProductModel->create($product);
+        redirect('dashboard');
+    }
+
+    public function deleteProduct($product_id) {
+        $this->ProductModel->delete($product_id);
+        redirect('dashboard');
+    }
+}
--- application/views/products/product_add.php
+++ application/views/products/product_add.php
@@ -1,6 +1,8 @@
+
     <nav class="navbar navbar-light navbar-expand-md fixed-top">
         <div class="container-fluid">
-            <a href="<?= base_url('home'); ?>"><img class="img-fluid" src="<?= base_url('assets/img/Logo%20exfest.png'); ?>" width="100"></a>
+            <a href="<?= base_url('home'); ?>">
+            <img class="img-fluid" src="<?= base_url('assets/img/Logo%20exfest.png'); ?>" width="100"></a>
             <button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1">
                 <span class="sr-only">Toggle navigation</span>
                 <span class="navbar-toggler-icon"></span>
@@ -16,31 +18,34 @@
     </nav>
 
     <div class="container" id="content">
-        <h2 class="text-center">Stand</h2>
-        <form>
-            <div class="form-group">
-                <label>Nama Stand</label>
-                <input class="form-control" type="text" name="nama-stand">
+        <h2 class="text-center"><?= $user['name']; ?></h2>
+        <?php foreach ($products as $product): ?>
+        <a href="#">
+            <div class="card mt-2 bg-light">
+                <div class="card-body">
+                    <h4 class="card-title"><?= $product['name']; ?></h4>
+                    <h6 class="text-muted card-subtitle mb-2">Rp. <?= $product['price']; ?></h6>
+                </div>
             </div>
-            <hr>
+        </a>
+        <?php endforeach; ?>
+        <hr>
+
+        <form class="mt-3" method="POST" action="<?= base_url('dashboard/addproduct/') . $user['id']; ?>">
             <div class="form-group">
                 <label>Nama Produk</label>
-                <input class="form-control" type="text" name="makanan1">
+                <input class="form-control" type="text" name="name">
             </div>
             <div class="form-group">
                 <label>Harga</label>
-                <input class="form-control" type="text" name="harga-makanan1">
+                <input class="form-control" type="text" name="price">
             </div>
-            <div class="form-group">
-                <label>Tempat</label>
-                <input type="file">
-            </div>
-            <hr>
             <div class="text-center mb-5">
-                <button class="btn btn-primary mr-1" type="button" data-toggle="modal" data-target="#modal-simpan">Simpan</button><a class="btn btn-outline-danger ml-1" href="dashboard.html">Kembali</a>
+                <button class="btn btn-primary mr-1" type="button" data-toggle="modal" data-target="#modal-simpan">Simpan</button>
+                <a class="btn btn-outline-danger ml-1" href="<?= base_url('dashboard'); ?>">Kembali</a>
             </div>
-            <div class="modal fade" role="dialog" tabindex="-1" id="modal-simpan"
-                aria-hidden="true">
+            <div class="modal fade" role="dialog" tabindex="-1"
+                id="modal-simpan" aria-hidden="true">
                 <div class="modal-dialog" role="document">
                     <div class="modal-content">
                         <div class="modal-header">
@@ -51,7 +56,8 @@
                             <p>Pilih batal jika data ingin membatalkan.</p>
                         </div>
                         <div class="modal-footer">
-                            <button class="btn btn-primary" type="submit">Simpan</button><button class="btn btn-light" type="button" data-dismiss="modal">Batal</button>
+                            <button class="btn btn-primary" type="submit">Simpan</button>
+                            <button class="btn btn-light" type="button" data-dismiss="modal">Batal</button>
                         </div>
                     </div>
                 </div>
